36861bbd692065a01442d021650c70b762fb3023
Dariusssssssss <sablezzubaya@gmail.com>
2025-01-07 01:45:13 +0300

    add: task solution "matrix elements sum", "recursive depth"

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,10 +16,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum(matrix) {
+  let sum = 0;
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+
+  for (let c = 0; c < cols; c++) {
+    for (let r = 0; r < rows; r++) {
+      if (r === 0 || matrix[r - 1][c] !== 0) {
+        sum += matrix[r][c];
+      } else {
+        break;
+      }
+    }
+  }
+  return sum;
 }
+
 
 module.exports = {
   getMatrixElementsSum
