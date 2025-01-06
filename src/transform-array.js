ef6e5aa85520a02ab30d76c7be7f3a0598bb34e6
Dariusssssssss <sablezzubaya@gmail.com>
2025-01-07 02:33:51 +0300

    add: task non-solution "transform array", "vigenere cipher"

--- src/transform-array.js
+++ src/transform-array.js
@@ -3,19 +3,48 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error("'arr' parameter must be an instance of the Array!");
+    }
+
+    const result = [];
+    const len = arr.length;
+
+    for (let i = 0; i < len; i++) {
+        const current = arr[i];
+
+        if (current === '--double-next') {
+            if (i + 1 < len) {
+                result.push(arr[i + 1]);
+            }
+        } else if (current === '--discard-prev') {
+            if (i > 0) {
+                result.pop();
+            }
+        } else if (current === '--discard-next') {
+            if (i + 1 < len) {
+                i++;
+            }
+        } else if (current === '--double-prev') {
+            if (i > 0) {
+                result.push(arr[i - 1]);
+            }
+        } else {
+            result.push(current);
+        }
+    }
+    return result;
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,30 +3,76 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    // Check if arguments are missing
+    if (message === undefined || key === undefined) {
+      throw new Error('Invalid arguments!');
+    }
+    let result = '';
+    let keyIndex = 0;
+    message = message.toUpperCase();
+    for (let i = 0; i < message.length; i++) {
+      let char = message[i];
+      if (/[a-zA-Z]/.test(char)) {
+        const shift = key[keyIndex % key.length].toLowerCase().charCodeAt(0) - 'a'.charCodeAt(0);
+        let charCode = char.charCodeAt(0);
+        if (/[a-z]/.test(char)) {
+          charCode = ((charCode - 'a'.charCodeAt(0) + shift) % 26) + 'a'.charCodeAt(0);
+        } else if (/[A-Z]/.test(char)) {
+          charCode = ((charCode - 'A'.charCodeAt(0) + shift) % 26) + 'A'.charCodeAt(0);
+        }
+        result += String.fromCharCode(charCode);
+        keyIndex++;
+      } else {
+        result += char;
+      }
+    }
+    return this.direct ? result : result.split('').reverse().join('');
+  }
+  decrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error('Invalid arguments!');
+    }
+    let result = '';
+    let keyIndex = 0;
+    message = message.toUpperCase();
+    for (let i = 0; i < message.length; i++) {
+      let char = message[i];
+      if (/[a-zA-Z]/.test(char)) {
+        const shift = key[keyIndex % key.length].toLowerCase().charCodeAt(0) - 'a'.charCodeAt(0);
+        let charCode = char.charCodeAt(0);
+        if (/[a-z]/.test(char)) {
+          charCode = ((charCode - 'a'.charCodeAt(0) - shift + 26) % 26) + 'a'.charCodeAt(0);
+        } else if (/[A-Z]/.test(char)) {
+          charCode = ((charCode - 'A'.charCodeAt(0) - shift + 26) % 26) + 'A'.charCodeAt(0);
+        }
+        result += String.fromCharCode(charCode);
+        keyIndex++;
+      } else {
+        result += char;
+      }
+    }
+    return this.direct ? result : result.split('').reverse().join('');
   }
 }
 
