b5a8f2fa22f283adb8599228bf93f7f46316b485
Dariusssssssss <sablezzubaya@gmail.com>
2025-11-11 01:26:29 +0300

    add: dream team solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const {NotImplementedError} = require('../lib');
 
 /**
  * Create name of dream team based on the names of its members
@@ -13,11 +13,18 @@ const { NotImplementedError } = require('../lib');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+    if (!Array.isArray(members)) return false;
+
+    let team = members
+        .filter(el => typeof el === "string")
+        .map(name => name.trim().charAt(0).toUpperCase())
+        .sort()
+        .join('');
+
+    return team.length > 0 ? team : false;
 }
 
 module.exports = {
-  createDreamTeam
+    createDreamTeam
 };
